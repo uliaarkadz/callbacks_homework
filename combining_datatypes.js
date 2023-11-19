3fe5f70aeffaeee2de414ad24006777b7bad8845
ybuiko <juliabuiko@gmail.com>
2023-11-18 22:03:29 -0500

    Model a Vending Machine Model a vending machine

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -71,3 +71,24 @@ const powerButton = () => {
   options();
 };
 powerButton();
+
+// Model a Vending Machine Model a vending machine
+
+// a vending machine is an object
+// it has an array of snacks (make 3 snacks)
+// snacks are objects that have a name and a price
+// a vending machine has a function vend that allows user to enter the array position (a number) of the snack and then that snack will be returned
+// Be able to call vendingMachine.vend() with a valid integer to return a snack
+
+const vendingMachine = {
+  snacks: [
+    { name: "chips", price: 1.5 },
+    { name: "candy", price: 2 },
+    { name: "crackers", price: 2.5 },
+  ],
+  vend: function (num) {
+    return this.snacks[num];
+  },
+};
+
+console.log(vendingMachine.vend(2));
